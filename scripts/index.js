d6590e97cbfe2743f5ce37a68334cfb66ab5d59b
Margarita Kaerlieva <margaritaaverckina@yandex.ru>
2024-05-12 14:42:52 +0300

    дбавлены карточки и удаление

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,38 @@
 
 // @todo: DOM узлы
 
+const templateCard = document.querySelector('#card-template').content.querySelector('.places__item');
+const placesList = document.querySelector('.places__list');
+
+const handleDeleteCard = (card) => {
+    card.remove();
+}
+
 // @todo: Функция создания карточки
 
+const createCard = (cards, onDelete) => {
+    const newCard = templateCard.cloneNode(true);
+
+    const titleCard = newCard.querySelector('.card__title');
+    const imgCard = newCard.querySelector('.card__image');
+    const deleteButton = newCard.querySelector('.card__delete-button');
+
+    titleCard.textContent = cards.name;
+    imgCard.src = cards.link;
+    imgCard.alt = 'Изображение пейзажа';
+
+    deleteButton.addEventListener('click', () => {
+        onDelete(newCard);
+    })
+
+    return newCard;
+}
+
+initialCards.forEach((cards) => {
+    const newCard = createCard(cards, handleDeleteCard);
+
+    placesList.append(newCard);
+})
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
